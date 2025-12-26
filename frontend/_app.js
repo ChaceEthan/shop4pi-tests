7d9e16ac7d12be4d04485af8ffa93a5692de0478
ChaceEthan <nzabandoraisiac@gmail.com>
2025-12-26 07:51:54 +0000

    Update _app.js

--- frontend/_app.js
+++ frontend/_app.js
@@ -1,14 +1,11 @@
 import { useEffect } from "react";
-
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     if (typeof window === "undefined") return;
-
-    const script = document.createElement("script");
+const script = document.createElement("script");
     script.src = "https://sdk.minepi.com/pi-sdk.js";
     script.async = true;
-
-    script.onload = () => {
+script.onload = () => {
       if (window.Pi) {
         window.Pi.init({
           version: "2.0",
@@ -18,12 +15,8 @@ function MyApp({ Component, pageProps }) {
         });
       }
     };
-
-    document.body.appendChild(script);
+document.body.appendChild(script);
     return () => document.body.removeChild(script);
   }, []);
-
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
+return <Component {...pageProps} />;
+} export default MyApp;
